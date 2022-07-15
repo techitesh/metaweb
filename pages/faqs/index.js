ab28bdd6b87be7b510b228779d8f25c921004dbf
techitesh <fullstack.hitesh@gmail.com>
2022-07-15 17:06:50 +0530

    updated fixes

--- pages/faqs/index.js
+++ pages/faqs/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import BreadcrumbSection from "../../components/faqs/BreadcrumbSection";
 import MobileContactSection from "../../components/faqs/MobileContactSection";
-import StartFaqSection from "../../components/faqs/startFaqSection";
+import StartFaqSection from "../../components/faqs/StartFaqSection";
 const faqs = () => {
   return (
     <Fragment>
